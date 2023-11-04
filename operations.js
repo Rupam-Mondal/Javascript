51dc64d6d90b96b0bcc4e130ac22de3ef07fa5ad
Rupam <126224914+Rupam-Mondal@users.noreply.github.com>
2023-11-04 07:09:00 +0000

    upload oparation file

---
+++ operations.js
@@ -0,0 +1,14 @@
+const numone = "2"
+const numtwo = "3"
+
+//Arithmatic operation
+// add
+// multiply etc
+const numsthree = numone + numtwo
+console.log(numsthree)
+let valone = 3
+valone++
+console.log(valone)
+//Assignment operator
+// = is a assignment operator
+let valtwo = 3
