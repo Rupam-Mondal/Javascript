9971ff3282f818bff5b885e9f5743b64bf82ccea
Rupam <126224914+Rupam-Mondal@users.noreply.github.com>
2023-11-04 06:17:36 +0000

    data type

---
+++ data-type.js
@@ -0,0 +1,24 @@
+//primitive(STACK)
+const firstname = "Rupam"
+let loggedin = false
+const score = 22
+
+//Reference (non-primitive) - Objects(HEAP)
+// Arrays
+// objects
+// functions
+
+const numbers = [1 , 2 , 3 , 4 , 5]
+const heros = ["superman" , "batman"]
+
+const userobject = {
+    name : "Rupam",
+    age : 20,
+    isLogged : true
+}
+//function
+const sayHello = function(){
+    console.log("hitesh")
+}
+
+const register = function(){}
