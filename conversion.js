695dcde550c9805d6f358733ca6e55c202e832d5
Rupam <126224914+Rupam-Mondal@users.noreply.github.com>
2023-11-05 05:08:48 +0000

    upload conversion.js

---
+++ conversion.js
@@ -0,0 +1,15 @@
+let bankBalance = '-1'
+
+let bankBalanceInt = Number(bankBalance);
+
+// console.log(typeof bankBalance)
+// console.log(typeof bankBalanceInt)
+
+console.log(bankBalance)
+console.log(typeof bankBalanceInt)
+//empty value in string is cosidered as false and non empty string is true
+
+//interview question
+console.log(NaN == NaN) //answer is false
+console.log(5 + null) // answer is 5
+console.log(5 + undefined) //answer is NaN
