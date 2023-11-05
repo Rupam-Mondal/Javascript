a9ad88cd99af0390b7313009debd909b403a13ed
Rupam <126224914+Rupam-Mondal@users.noreply.github.com>
2023-11-05 05:41:54 +0000

    Number session

--- numAndString.js
+++ numAndString.js
@@ -14,3 +14,19 @@ console.log(username.substring(0 , 3))
 let email = "Rupam@gmail.com"
 console.log(email.replace("Ru" , "Baklol"))
 console.log(email.includes("R"))
+
+
+
+//Numbers
+//const num = 5 short hand declaration
+//behind the scene const num = new Number(5)
+const num = new Number(5.3456)
+console.log(num.toFixed(2))
+
+const subcribers =100000
+console.log(subcribers.toLocaleString())
+console.log(Number.MAX_VALUE)
+
+//we can do any math operation by writiing Math.()
+
+console.log(Math.floor(Math.random() * 10))
