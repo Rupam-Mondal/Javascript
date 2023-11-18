9cfd1dbaf62ca252f2d174dbe6c4c106b77544a1
Rupam <126224914+Rupam-Mondal@users.noreply.github.com>
2023-11-18 06:45:13 +0000

    switch case

--- control2/01-if.js
+++ control2/01-if.js
@@ -56,17 +56,5 @@ else if(star == 7){
     console.log("Nailed it")
 }
 
-//switch case
-let user = 5;
-switch(user){
-    case 1:
-        console.log("User value is 1")
-        break;
-    case 2:
-        console.log("user value is 2");
-        break;
-    default:
-        console.log("All good");
-}
 
 
---
+++ control2/02-switch.js
@@ -0,0 +1,11 @@
+let user = 5;
+switch(user){
+    case 1:
+        console.log("User value is 1")
+        break;
+    case 2:
+        console.log("user value is 2");
+        break;
+    default:
+        console.log("All good");
+}
