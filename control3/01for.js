2e58a9552c2e0116c94af5b28979845fbe2dc1cc
Rupam <126224914+Rupam-Mondal@users.noreply.github.com>
2023-11-19 06:40:12 +0000

    Basic loops

---
+++ control3/01for.js
@@ -0,0 +1,11 @@
+// for([initial Expression/ Declaration(1) -- only one time] ; [condition](2) ; [Changer/task](4))
+// {
+    // body(3)
+// }
+
+
+
+const array =[1 , 2 , 3 , 4 , 5]
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
