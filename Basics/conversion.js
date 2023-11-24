b8e3fb3f70848e04ce8e3a290a585b102391efab
Rupam <126224914+Rupam-Mondal@users.noreply.github.com>
2023-11-24 11:09:34 +0000

    conversion practice

--- Basics/conversion.js
+++ Basics/conversion.js
@@ -13,8 +13,11 @@ console.log(typeof bankBalanceInt)
 console.log(NaN == NaN) //answer is false
 console.log(5 + null) // answer is 5
 console.log(5 + undefined) //answer is NaN
+console.log(null == undefined) //true
 
 const a = "2";
 const b = parseInt(a)
 console.log(typeof a);
 console.log(typeof b);
+console.log(typeof NaN);
+console.log(typeof null);
