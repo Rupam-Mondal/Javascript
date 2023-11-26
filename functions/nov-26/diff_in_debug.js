385b487359ee7351db63353546369ea98687258a
Rupam <126224914+Rupam-Mondal@users.noreply.github.com>
2023-11-26 06:07:56 +0000

    minor changes

--- functions/nov-26/diff_in_debug.js
+++ functions/nov-26/diff_in_debug.js
@@ -1,5 +1,6 @@
 const isEvenorodd = function process(num) {
-    // console.trace("Trace of the function calling") // this gives us the the sequence of func calling
+    // console.trace("Trace of the function calling") 
+    // this gives us the the sequence of func calling
     return num % 2 == 0;
 }
 
@@ -9,6 +10,6 @@ const namedFunction = function exec() {
 
 const anonymousFunction = function exec() {
     const res = isEvenorodd(11);
-}
+}                                           
 const result = namedFunction()
 console.log();
