63ad7ec31014e4fb74c683a9674d56808d02efcb
Rupam <rupammondal564@email.com>
2023-12-02 11:14:40 +0530

    return a function

---
+++ functions/Dec_02/function_return.js
@@ -0,0 +1,12 @@
+function fun() {
+    console.log("Called the function fun");
+
+
+    return function cleanup() {
+        console.log("Cleaning up the resources");
+    }
+}
+
+
+const text = fun();
+text()
