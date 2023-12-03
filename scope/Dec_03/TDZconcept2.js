8618fe70750394873f5fe95f5a8663b2da9b851e
Rupam <rupammondal564@email.com>
2023-12-03 17:39:38 +0530

    TDZ concept

---
+++ scope/Dec_03/TDZconcept2.js
@@ -0,0 +1,13 @@
+function fun() {
+    console.log(x);
+    let x = 10;
+    console.log(x);
+}
+function gun() {
+    console.log(x);//Temporal dead zone
+    let x = 10;
+    console.log(x);
+}
+
+fun();
+gun();
