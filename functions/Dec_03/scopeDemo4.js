3e01a433a106be26c9c93c2677050ff662592b65
Rupam <rupammondal564@email.com>
2023-12-03 15:03:41 +0530

    more clear file about auto globals

---
+++ functions/Dec_03/scopeDemo4.js
@@ -0,0 +1,23 @@
+var teacher = "Rupam"; // global scope
+
+function fun() {
+    var teacher = "Anurag"; // fun scope
+    content = "js";
+    //content is not in the fun scope it is in the global scope the method is known as (auto globals)
+
+
+    console.log("Hello ," , teacher);
+}
+
+function gun() {
+    var student = "Karthik";
+    console.log("Welcome to the class", student , teacher); //gun scope
+    console.log(content);
+}
+
+console.log(teacher , content); //this will throw a error
+
+fun();
+gun();
+
+console.log(teacher , content);
