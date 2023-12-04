fb695f43f866f746f05011eaee9ed1fabc810c00
Rupam <rupammondal564@email.com>
2023-12-04 17:18:22 +0530

    IFEE

---
+++ self-practice/functions/IFEE.js
@@ -0,0 +1,19 @@
+// function greet() {
+//     console.log("Hello bro");
+// }
+
+// greet()
+
+// function greet() {
+//     console.log("Hello");
+// }
+
+
+(function add(a , b) {
+    console.log(a + b);
+}) (4 , 3);
+
+
+(function greet() {
+    console.log("Hi bro");
+}) ();
