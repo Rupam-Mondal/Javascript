95bd211c3196b3e9a0457806efd3dd1d7e162270
Rupam <rupammondal564@email.com>
2023-12-10 11:01:41 +0530

    operators

---
+++ 10_dec/equality.js
@@ -0,0 +1,11 @@
+//diff between == and ===
+//https://262.ecma-international.org/10.0/#sec-strict-equality-comparison
+//go to this link for better difference
+
+
+
+console.log(11 == "11"); // coercion process is being done
+console.log(11 === "11"); //no coercion process is being done
+
+const x = {a : 10 , b : 20}
+console.log("Rupam" + x); // why this type of behaviour explore + in
---
+++ 10_dec/number_related_function.js
@@ -0,0 +1,12 @@
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+
+//convert a decimal number to binary
+const x = 13
+console.log(x.toString(2)); //inside the method base of number
+console.log(x.toString(8)); //octal number
+//if you call without argument then it just converts to string
+
+//binary to decimal
+console.log(parseInt("1101"  , 2)); // second argument is base
