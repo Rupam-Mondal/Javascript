4d488fdf8d600c5b48c717ca51b838a5462537fe
Rupam <rupammondal564@email.com>
2023-12-12 09:04:43 +0530

    something about numbers

--- 10_dec/Date.js
+++ 10_dec/Date.js
@@ -14,3 +14,15 @@ console.log(dt.getDay());
 
 //This below method is very special it return total second from 1st jan of 1970
 console.log(dt.getTime());
+
+
+
+//Big integer in js two ways to make big int
+//after a big number just write a small n 6675........90n then it will be the okay
+//also
+const big = BigInt("4453456788909876545678998765456789009876543456909876543212345678909876543")
+console.log(big);
+
+///check output
+console.log(0n == 0);
+console.log(0n === 0);
