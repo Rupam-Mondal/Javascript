3e6d661b61911af5e1db8e93af38bb66a2251a67
Rupam <rupammondal564@email.com>
2024-01-11 13:12:02 +0530

    thread discussion

---
+++ Asynchronous_prog/Async_code_ex.js
@@ -0,0 +1,11 @@
+console.log("Hey bro");
+
+console.log("Starting to excecute");
+
+setTimeout(() => {
+    console.log("I am from set timeout function");
+} , 5000); // this execute the callback after 5000 milisecond
+
+// after running you will see some magic
+
+console.log("End");
---
+++ Asynchronous_prog/sync_code_ex.js
@@ -0,0 +1,11 @@
+//this is a example of synchronous programming
+
+console.log("Hey bro");
+
+console.log("Starting to excecute");
+
+for(let i = 0 ; i < 1000000000000 ; i++){
+    //some time taking steps
+}
+
+console.log("End");
