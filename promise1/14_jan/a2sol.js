3f1d1056892aa423828c8248743a3b2acf1a6886
Rupam <rupammondal564@gmail.com>
2024-01-26 11:11:25 +0530

    Ass2 solution done

---
+++ promise1/14_jan/a2sol.js
@@ -0,0 +1,36 @@
+function downloader(url, cb) {
+    console.log("Started downloading from url" , url);
+    setTimeout(() => {
+        console.log("Downloaded");
+        let data = "some content";
+        cb(data); 
+    } , 4000);
+}
+
+function writeFile(data, cb) {
+    console.log("Started downloading from url");
+    setTimeout(() => {
+        console.log("Writing completed");
+        let text = "some text";
+        cb(text);
+    }, 2000);
+}
+
+function uploadFile(fileName, newUrl, cb) {
+    console.log("Started uploading " , fileName);
+    setTimeout(() => {
+        console.log("Upload completed");
+        let responce = "success";
+        cb(responce);
+    }, 3000);
+}
+
+console.log("Start the process");
+
+downloader("www.google.com" , (data) => {
+    writeFile(data , (text) => {
+        uploadFile("Something.txt" , "Youtube.com" , (responce) => {
+            console.log(responce);
+        })
+    })
+})
