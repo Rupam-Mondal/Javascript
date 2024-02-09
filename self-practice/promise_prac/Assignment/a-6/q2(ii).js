0b885aeba9944215753855a9da24e0f64b6ea11a
Rupam <rupammondal564@gmail.com>
2024-02-09 12:51:26 +0530

    concurrent fetching with promise and performence measure

--- self-practice/promise_prac/Assignment/a-6/q2(ii).js
+++ self-practice/promise_prac/Assignment/a-6/q2(ii).js
@@ -1,16 +1,38 @@
-function fetchSequestially(arr) {
-    console.log("Fetching started");
-    const pr1 = fetch(arr[0]);
-    pr1.then(() => {
-        return fetch(arr[1]);
-    }).then(() => {
-        return fetch(arr[2]);
-    }).then(() => {
 
+function fetchFromGoogle(url) {
+    return new Promise((res , rej) => {
+        setTimeout(() => {
+            res("Downloaded from ", url);
+        } , 2000)
     })
+}
+function fetchFromFlipkart(url) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res("Downloaded from ", url);
+        }, 2000)
+    })
+}
+
+function fetchFromAmazon(url) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res("Downloaded from ", url);
+            
+        }, 2000)
+    })
+}
+
+function concurrently(arr) {
+    return Promise.all([fetchFromGoogle(arr[0]) , fetchFromFlipkart(arr[1]) , fetchFromAmazon(arr[2])]);
 }
 const arr = [
     "www.google.com",
     "www.flipkart.com",
     "www.amazon.com"
 ]
+
+concurrently(arr).then(() => {
+    console.log("fetched data successfully");
+    console.log(performance.now());
+})
