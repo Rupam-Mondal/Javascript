6de38e8d8bb9c726934871c9277ea2e5d09a5f93
Rupam <rupammondal564@gmail.com>
2024-02-16 16:37:35 +0530

    sleep question solved in leetcode

---
+++ self-practice-1/sleep.js
@@ -0,0 +1,10 @@
+// Given a positive integer millis, write an asynchronous function that
+//  sleeps for millis milliseconds.It can resolve any value.
+
+async function sleep(millis) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res();
+        }, millis);
+    })
+}
